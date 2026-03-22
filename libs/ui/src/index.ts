0d55fb2d10cc53e8848ab2c48032827bca345cb8
dqube <mdevendran@outlook.com>
2026-03-22 01:00:16 -0300

    Initial commit

---
+++ libs/ui/src/index.ts
@@ -0,0 +1,41 @@
+// Primitives (shadcn/ui base)
+export * from "./components/button";
+export * from "./components/input";
+export * from "./components/label";
+export * from "./components/badge";
+export * from "./components/card";
+export * from "./components/separator";
+export * from "./components/avatar";
+export * from "./components/skeleton";
+export * from "./components/spinner";
+
+// Overlays
+export * from "./components/dialog";
+export * from "./components/popover";
+export * from "./components/dropdown-menu";
+export * from "./components/sheet";
+export * from "./components/tooltip";
+
+// Toast (sonner)
+export * from "./components/toast";
+
+// Forms
+export * from "./components/checkbox";
+export * from "./components/select";
+export * from "./components/switch";
+export * from "./components/textarea";
+
+// Layout
+export * from "./components/tabs";
+export * from "./components/scroll-area";
+export * from "./components/stepper";
+
+// Theme
+export * from "./components/theme-provider";
+export * from "./components/theme-toggle";
+
+// Data Grid (reui-style, TanStack Table)
+export * from "./components/data-grid/index";
+
+// Utils
+export { cn } from "./lib/utils";
